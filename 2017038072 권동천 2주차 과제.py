5efaf3983b1fa1325a8d746a934f458532c991f2
Movesky98 <62997633+Movesky98@users.noreply.github.com>
2020-06-23 14:27:09 +0900

    Create 2017038072 권동천 2주차 과제.py

---
+++ 2017038072 권동천 2주차 과제.py
@@ -0,0 +1,8 @@
+Participants = int(input("참여자의 수를 입력하시오: "))
+Chicken = Participants
+Beer = Participants * 2
+Cake = Participants * 4
+
+print("치킨의 수 : %d 마리" % Chicken)
+print("맥주의 수 : %d 캔" % Beer)
+print("케익의 수 : %d 조각" % Cake)
